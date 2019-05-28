c2d1b1b21b0dd31c762e31b02ba6e1726dc5939b
mbalazs98 <49280623+mbalazs98@users.noreply.github.com>
2019-05-28 14:38:14 +0200

    Create weight_tuning.py

---
+++ RadioProject/Balázs/weight_tuning.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+from io_operations import DataLoader
+from classifier_orchestrator import ClassifierOrchestrator
+
+#This function plots how the accuracies changes by adjusting the weights from 0 to 1. It excepts an array of standard weights, and it plots multiple times, always changing the weight of one prediction.
+def plot_vote_weight_acc(data_type,preds,std_weights):
+	y=load_test_data(data_type)['class'].tolist()
+	total=len(preds)
+	if data_type=='music':
+		t=b'music'
+		f=b'no_music'
+		ind=0
+	else:
+		t=b'speech'
+		f=b'no_speech'
+		ind=1
+	col=['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+	for i in range(len(std_weights)):
+		acc_arr=[]
+		for j in range(0,1,0.1):
+			tmp_weights=std_weights
+			tmp_weights[i]=j
+			acc=0
+			for k in range(len(preds)):
+				fin=get_final_prediction_for_one_instance_weighed(preds[k],tmp_weights)[ind]
+				if (fin==1 and y[k]==t) or (fin==0 and y[k]==f):
+					acc=acc+1
+			acc_arr.append(acc/total)
+		plt.plot(range(0,1,0.1),acc_arr,col[i])
+	plt.show()
+
+'''
+#This function shows that by adjusting the weights, how much do the classifiers agree. Maximize this.
+def try_weigths(preds, weights):
+	final=[]
+	t_all=0
+	f_all=0
+	for i in preds:
+		t=0
+		f=0
+		for j in preds[i]:
+			if preds[i][j]==1:
+				t=t+weights[j]
+				t_all=t_all+weights[j]
+			else:
+				f=f+weights[j]
+				f_all=f_all+weights[j]
+		final.add(abs(t-f))
+	t_all=abs(t_all-f_all)/len(final)
+	return final,t_all
+'''
